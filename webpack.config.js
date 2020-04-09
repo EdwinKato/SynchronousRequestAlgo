61f94328386e9adb16dc32f240965b67cba06d38
Edwin <edwin.kato@atlassiancommunity.com>
2020-04-09 23:06:13 +0300

    Add linting to the project

--- webpack.config.js
+++ webpack.config.js
@@ -6,58 +6,60 @@ const config = require('config');
 /*-------------------------------------------------*/
 
 module.exports = {
-    // webpack optimization mode
-    mode: ( process.env.NODE_ENV ? process.env.NODE_ENV : 'development' ),
+  // webpack optimization mode
+  mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
 
-    // entry file(s)
-    entry: './src/index.js',
+  // entry file(s)
+  entry: './src/index.js',
 
-    // output file(s) and chunks
-    output: {
-        // library: 'UserList',
-        library: 'makeRequest',
-        libraryTarget: 'umd',
-        globalObject: '(typeof self !== "undefined" ? self : this)',
-        libraryExport: 'default',
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index.js',
-        publicPath: config.get('publicPath')
-    },
+  // output file(s) and chunks
+  output: {
+    // library: 'UserList',
+    library: 'makeRequest',
+    libraryTarget: 'umd',
+    globalObject: '(typeof self !== "undefined" ? self : this)',
+    libraryExport: 'default',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    publicPath: config.get('publicPath'),
+  },
 
-    // module/loaders configuration
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [{'plugins': ['@babel/plugin-proposal-class-properties']}]
-                }
-            },
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-            }
-        ]
-    },
-
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html')
-        })
+  // module/loaders configuration
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [{ plugins: ['@babel/plugin-proposal-class-properties'] }],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      },
     ],
+  },
 
-    // development server configuration
-    devServer: {
-        
-        // must be `true` for SPAs
-        historyApiFallback: true,
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
 
-        // open browser on server start
-        open: config.get('open')
-    },
+  // development server configuration
+  devServer: {
+    // must be `true` for SPAs
+    historyApiFallback: true,
 
-    // generate source map
-    devtool: ( 'production' === process.env.NODE_ENV ? 'source-map' : 'cheap-module-eval-source-map' ),
+    // open browser on server start
+    open: config.get('open'),
+  },
+
+  // generate source map
+  devtool:
+    process.env.NODE_ENV === 'production'
+      ? 'source-map'
+      : 'cheap-module-eval-source-map',
 };
